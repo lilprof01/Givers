9bb1c92736508989b00c9b10793f038c99a31fa8
lilprof01 <adewumime@gmail.com>
2025-06-29 19:58:18 +0100

    dashboard changes in progress

--- src/Components/Dashboard/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar.jsx
@@ -2,7 +2,16 @@ import Menu from "./Menu";
 import logo from "/assets/images/logo.png";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { Home, Package, Search, Bell, Settings, LogOut, HandCoins, HandHeart } from "lucide-react";
+import {
+  Home,
+  Package,
+  Search,
+  Bell,
+  Settings,
+  LogOut,
+  HandCoins,
+  HandHeart,
+} from "lucide-react";
 
 const Sidebar = ({
   isCollapsed,
@@ -13,37 +22,45 @@ const Sidebar = ({
 }) => {
   return (
     // sidebar for dashboard page
-    <aside className={`bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 row-span-2 transition-all duration-300 sm:flex flex-col justify-between pt-5 ${
+    <aside
+      className={`bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 row-span-2 transition-all duration-300 sm:flex flex-col justify-between pt-5 ${
         window.innerWidth < 768 ? "fixed w-[250px] h-full z-[999]" : ""
-      } ${window.innerWidth < 768 && isCollapsed ? '-translate-x-[250px]' : ''}`}>
+      } ${
+        window.innerWidth < 768 && isCollapsed ? "-translate-x-[250px]" : ""
+      }`}
+    >
       <div
         className={`flex ${
           isCollapsed ? "" : ""
         } items-center align-middle transition-all duration-300`}
       >
-        {/* <div
-          onClick={() => setIsCollapsed(!isCollapsed)}
-          className="flex flex-col justify-center items-center align-middle gap-1 p-4 hover:cursor-pointer"
-        >
-          <div className="h-1 w-8 bg-purple-900"></div>
-          <div className="h-1 w-8 bg-purple-900"></div>
-          <div className="h-1 w-8 bg-purple-900"></div>
-        </div> */}
-
-        
-          <Link to="/" className="mr-2 transition-all duration-300 ml-2">
-            <div className="flex items-center mb-6">
-              <div className={`${isCollapsed ? 'ml-2' : ''} inline-flex items-center justify-center w-12 h-12 bg-gradient-to-br from-blue-500 to-green-500 dark:from-blue-600 dark:to-green-600 rounded-xl mr-2 transition-all duration-500`}>
-                <HandHeart className="text-white" />
-              </div>
-              <div className={``}>
-                <h3 className={`${isCollapsed ? 'text-[0px]' : 'text-2xl'} font-bold bg-clip-text bg-gradient-to-r from-black dark:from-blue-500 via-blue-400 to-green-400 text-transparent transition-all duration-500`}>
-                  Givers
-                </h3>
-                <p className={`${isCollapsed ? 'text-[0px]' : 'text-xs'} font-light transition-all duration-500`}>Dashboard</p>
-              </div>
+        <Link to="/" className="mr-2 transition-all duration-300 ml-2">
+          <div className="flex items-center mb-6">
+            <div
+              className={`${
+                isCollapsed ? "ml-2" : ""
+              } inline-flex items-center justify-center w-12 h-12 bg-gradient-to-br from-blue-500 to-green-500 dark:from-blue-600 dark:to-green-600 rounded-xl mr-2 transition-all duration-500`}
+            >
+              <HandHeart className="text-white" />
             </div>
-          </Link>
+            <div className={``}>
+              <h3
+                className={`${
+                  isCollapsed ? "text-[0px]" : "text-2xl"
+                } font-bold bg-clip-text bg-gradient-to-r from-black dark:from-blue-500 via-blue-400 to-green-400 text-transparent transition-all duration-500`}
+              >
+                Givers
+              </h3>
+              <p
+                className={`${
+                  isCollapsed ? "text-[0px]" : "text-xs"
+                } font-light transition-all duration-500`}
+              >
+                Dashboard
+              </p>
+            </div>
+          </div>
+        </Link>
       </div>
       <div className="py-8 px-2 h-[80%] flex flex-col justify-between align-middle">
         {MenuLinks.map((menu) => (
@@ -62,17 +79,27 @@ const Sidebar = ({
         onClick={() => handleSelectedMenu("profile")}
         className={`h-[10%] rounded-tr-2xl rounded-tl-2xl flex p-3 items-center align-middle gap-2 hover:cursor-pointer border-t`}
       >
-        <div className={`${isCollapsed ? 'ml-2' : ''} h-10 w-10 bg-gradient-to-r from-blue-400 to-green-400 dark:from-blue-600 dark:to-green-600 rounded-full flex items-center justify-center text-white transition-all duration-500`}>
+        <div
+          className={`${
+            isCollapsed ? "ml-2" : ""
+          } h-10 w-10 bg-gradient-to-r from-blue-400 to-green-400 dark:from-blue-600 dark:to-green-600 rounded-full flex items-center justify-center text-white transition-all duration-500`}
+        >
           AP
         </div>
 
-        <div className={`${isCollapsed ? 'text-[0px]' : ''} transition-all duration-500`}>
-          <p
-            className={`font-semibold`}
+        <div
+          className={`${
+            isCollapsed ? "text-[0px]" : ""
+          } transition-all duration-500`}
+        >
+          <p className={`font-semibold`}>Aniyajuwon Pelumi</p>
+          <a
+            className={`${
+              isCollapsed ? "text-[0px]" : "text-xs"
+            } font-light transition-all duration-500`}
           >
-            Aniyajuwon Pelumi
-          </p>
-          <a className={`${isCollapsed ? 'text-[0px]' : 'text-xs'} font-light transition-all duration-500`}>adewumime@gmail.com</a>
+            adewumime@gmail.com
+          </a>
         </div>
       </div>
     </aside>
